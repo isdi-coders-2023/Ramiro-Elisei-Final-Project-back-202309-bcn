55b5fbe92ec091236d2cd71ff4805e6e01a02f92
ramiro elisei <ramiroelisei@gmail.com>
2023-11-27 18:32:38 +0100

    Add app to server

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,15 @@
+import "dotenv/config";
+import chalk from "chalk";
 import express from "express";
+import debugCreator from "debug";
 
-const app = express();
+const debug = debugCreator("ludic:server:app");
+
+export const app = express();
+app.disable("x-powered-by");
 
 export const startServer = (port: number) => {
-  app.listen(port, () => {
-    // Mensaje para cuando se ha levantado 👋
+  app.listen(+port, () => {
+    debug(chalk.green(`Server is listening on http://localhost:${port}`));
   });
 };
-
-export default app;
