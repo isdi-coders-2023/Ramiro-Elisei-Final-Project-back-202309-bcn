492505bccf66d666075706aa2eee43a078435317
ramiro elisei <ramiroelisei@gmail.com>
2023-11-29 11:16:02 +0100

    Add jest coverage ignore pathern

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,4 +4,9 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
+  coveragePathIgnorePatterns: [
+    "src/index.ts",
+    "src/database/index.ts",
+    "src/server/app.ts",
+  ],
 };
