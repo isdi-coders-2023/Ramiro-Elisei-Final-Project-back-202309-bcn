30c3d7193e5a054a4340d074a57e07a76e103e77
ramiro elisei <ramiroelisei@gmail.com>
2023-12-06 09:33:24 +0100

    Add activity delete router test

---
+++ src/features/activities/mocks/activitiesMockId.ts
@@ -0,0 +1,26 @@
+import { type ActivityStructureId } from "../types";
+
+export const activitiesMockId: ActivityStructureId[] = [
+  {
+    _id: "656493fa44b5521c2584c216",
+    activityType: "Workshop",
+    activityName: "Generative plant gadering",
+    ledBy: "Steve Green and Lia Rasmussen",
+    date: "27/12/2023",
+    startTime: "15:00",
+    activityDescription:
+      "Cultivate sustainable ideas in our playful Generative Plant Gathering workshop: where creativity blossoms with environmental consciousness.",
+    imageUrl: "https://i.ibb.co/8zFCCQk/plant-gadering.webp",
+  },
+  {
+    _id: "656493fa44b5521c2584c217",
+    activityType: "Classes",
+    activityName: "Unplugged smoothies bike",
+    ledBy: "Caravelle bar",
+    date: "13:30",
+    startTime: "27/12/2023",
+    activityDescription:
+      "Unplug, pedal, blend! Join our Unplugged Smoothies Bike workshop for a fusion of fun, flavor, and fitness. Let's ride and blend our way to tasty triumphs!",
+    imageUrl: "https://i.ibb.co/gRbgwSs/Smoothie.webp",
+  },
+];
---
+++ src/features/activities/router/deleteActivityEndpoint.test.ts
@@ -0,0 +1,35 @@
+import { app } from "../../../server/app";
+import "../../../server/index";
+import request from "supertest";
+import "../../../setupTests";
+
+describe("Given a DELETE method to the endpoint /activities/delete/:activityId", () => {
+  describe("When it receives a request with a valid id", () => {
+    test("Then it should respond with status code 200 and an empty object", async () => {
+      const path = "/activities/delete/656493fa44b5521c2584c216";
+      const expectedStatusCode = 200;
+
+      const response = await request(app)
+        .delete(path)
+        .expect(expectedStatusCode);
+
+      expect(response.body).toStrictEqual({
+        message: "The activity has been deleted",
+      });
+    });
+  });
+
+  describe("When it receives a request with a non-existent id '656493fa44b55'", () => {
+    test("Then it should return a response with 400 status code and 'Error deleting activity'", async () => {
+      const expectedStatusCode = 400;
+      const path = "/activities/delete/656493fa44b55";
+      const expectErrorMessage = "Error deleting activity";
+
+      const response = await request(app)
+        .delete(path)
+        .expect(expectedStatusCode);
+
+      expect(response.body).toHaveProperty("error", expectErrorMessage);
+    });
+  });
+});
