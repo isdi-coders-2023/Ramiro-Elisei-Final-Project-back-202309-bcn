47cfdc5f1a74b378387d042fabc6951b690cc39e
ramiro elisei <ramiroelisei@gmail.com>
2023-12-06 09:26:28 +0100

    Add activities delete repository

--- src/features/activities/repository/ActivitiesMongooseRepository.ts
+++ src/features/activities/repository/ActivitiesMongooseRepository.ts
@@ -1,3 +1,4 @@
+import CustomError from "../../../server/CustomError/CustomError.js";
 import { Activity } from "../model/Activity.js";
 import { type ActivityStructure } from "../types";
 import { type ActivitiesRepository } from "./types";
@@ -7,6 +8,14 @@ class ActivitiesMongooseRepository implements ActivitiesRepository {
     const activities = await Activity.find().limit(10);
 
     return activities;
+  }
+
+  public async deleteActivity(activityId: string): Promise<void> {
+    try {
+      await Activity.findByIdAndDelete(activityId);
+    } catch (error) {
+      throw new CustomError("Error deleting activity", 400);
+    }
   }
 }
 
