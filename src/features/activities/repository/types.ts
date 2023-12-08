054af1db4b57c0efddb2a5eaff4fc210686c5d54
ramiro elisei <ramiroelisei@gmail.com>
2023-12-08 09:33:07 +0100

    Add activity repository

--- src/features/activities/repository/types.ts
+++ src/features/activities/repository/types.ts
@@ -1,6 +1,7 @@
-import { type ActivityStructure } from "../types";
+import { type ActivityData, type ActivityStructure } from "../types";
 
 export interface ActivitiesRepository {
   getActivities: () => Promise<ActivityStructure[]>;
   deleteActivity: (id: string) => Promise<void>;
+  addActivity: (activity: ActivityStructure) => Promise<ActivityData>;
 }
